99fbf3f2c3982873f7a00059265e941b0a06d477
Venkatesh-Dayani23 <128453924+Venkatesh-Dayani23@users.noreply.github.com>
2024-05-23 13:25:34 +0530

    Update JsonKey.java

--- core/platform-common/src/main/java/org/sunbird/keys/JsonKey.java
+++ core/platform-common/src/main/java/org/sunbird/keys/JsonKey.java
@@ -614,7 +614,7 @@ public final class JsonKey {
   public static final String OTP_CONTACT_UPDATE_TEMPLATE_SMS = "otpContactUpdateTemplateSms";
   public static final String OTP_DELETE_USER_TEMPLATE_SMS = "otpDeleteUserTemplateSms";
   public static final String CONTACT_DETAILS_UPDATE_VERIFICATION_SUBJECT =
-      "OTP to edit Diksha Profile";
+      "OTP to edit Your Profile";
   public static final String DELETE_USER_VERIFICATION_SUBJECT =
       "OTP to proceed with profile deletion.";
   public static final String X_Source = "x-source";
